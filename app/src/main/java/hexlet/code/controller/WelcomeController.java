2a468c3bd4fec6a59fb13eea0374e1e662ebd113
maksim butakov <qulifis@gmail.com>
2023-07-18 01:35:06 +0100

    Improve style

--- app/src/main/java/hexlet/code/controller/WelcomeController.java
+++ app/src/main/java/hexlet/code/controller/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-    @GetMapping(name = "/")
+    @GetMapping(path = "/")
     public String root() {
         return "Welcome to Spring";
     }
