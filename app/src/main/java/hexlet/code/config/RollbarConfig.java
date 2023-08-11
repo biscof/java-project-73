356991a81a6a8886ab2340b166a48c439d6da6d7
maksim butakov <qulifis@gmail.com>
2023-08-11 15:01:52 +0100

    Add Rollbar integration - 11

---
+++ app/src/main/java/hexlet/code/config/RollbarConfig.java
@@ -0,0 +1,44 @@
+package hexlet.code.config;
+
+import com.rollbar.notifier.Rollbar;
+import com.rollbar.notifier.config.Config;
+import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+
+@Configuration
+@ComponentScan({ "hexlet.code", "com.rollbar.spring" })
+public class RollbarConfig {
+
+    @Value("${ROLLBAR_TOKEN}")
+    private String rollbarToken;
+
+    private final Environment environment;
+
+    public RollbarConfig(Environment environment) {
+        this.environment = environment;
+    }
+    @Bean
+    public Rollbar rollbar() {
+        Rollbar rollbar = new Rollbar(getRollbarConfigs(rollbarToken));
+        rollbar.debug("Here is a test debug message");
+        return rollbar;
+    }
+
+    private Config getRollbarConfigs(String accessToken) {
+        String activeProfile = environment.getProperty("spring.profiles.active");
+        if (activeProfile.equals("dev") || activeProfile.equals("prod")) {
+            return RollbarSpringConfigBuilder.withAccessToken(accessToken)
+                    .environment(activeProfile)
+                    .build();
+        } else {
+            // If the active profile is other than "dev" or "prod" (say "test"),
+            // then don't use Rollbar
+            return RollbarSpringConfigBuilder.withAccessToken("").build();
+        }
+    }
+}
